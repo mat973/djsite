ae04718ecabd49420fbc2b44e37ddfe455a7dcd2
mat973 <48097405+mat973@users.noreply.github.com>
2022-06-04 10:15:11 +0300

    do something with url and js with css

---
+++ coolsite/women/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, re_path
+
+from .views import *
+
+urlpatterns = [
+    path('', index, name='home'),
+    path('about/', about, name='about'),
+
+    # path('cats/<slug:cat>/', categories),
+    # re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+
+]
