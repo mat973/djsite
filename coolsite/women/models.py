ffb90a0197482a442c583bca2445b9d6a65f28b0
mat973 <48097405+mat973@users.noreply.github.com>
2022-06-05 19:44:03 +0300

    slug instead of int

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -6,18 +6,19 @@ from django.urls import reverse
 # Create your models here.
 class Women(models.Model):
     title =models.CharField(max_length=255, verbose_name='заголовок')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
     content = models.TextField(blank = True, verbose_name='контент')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='фотография')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='время создания')
     time_update = models.DateTimeField(auto_now_add=True, verbose_name='вермя обовления')
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='категории')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='категории')
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('post', kwargs={'post_id': self.pk})
+        return reverse('post', kwargs={'post_slug': self.slug})
 #
     class Meta:
         verbose_name = 'Известные женщины'
@@ -27,6 +28,7 @@ class Women(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name='вид деятельности')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
 
 
     def __str__(self):
